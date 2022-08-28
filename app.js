3574adda1809a23e338d1bdd8ee3e9f5b669c2c0
mohammad <habibollahi24@gmail.com>
2022-08-28 19:05:29 +0430

    chakge h1

--- app.js
+++ app.js
@@ -7,11 +7,3 @@ menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("animation-btn");
   navbarMobile.classList.toggle("animation-nav");
 });
-
-// window.addEventListener("scroll", () => {
-//   if (window.scrollY > 50) {
-//     navigationMobile.classList.add("shadow");
-//   } else {
-//     navigationMobile.classList.remove("shadow");
-//   }
-// });
