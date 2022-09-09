de6b74b3a292845237d519a376c69a615b599893
mohammad <habibollahi24@gmail.com>
2022-09-09 13:04:11 +0430

    add animation

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const menuBtn = document.querySelector(".menu-btn");
 const navbarMobile = document.querySelector(".navbar-mobile");
 const navigationMobile = document.querySelector(".navigation-mobile");
-
+const my_work_on_page = document.getElementById("my-work-on-page");
+console.log(my_work_on_page);
 const secondLine = document.querySelector(".second-line");
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("animation-btn");
   navbarMobile.classList.toggle("animation-nav");
 });
+
+if (my_work_on_page)
+  my_work_on_page.addEventListener("click", () => {
+    console.log("salam");
+    menuBtn.classList.toggle("animation-btn");
+    navbarMobile.classList.toggle("animation-nav");
+  });
