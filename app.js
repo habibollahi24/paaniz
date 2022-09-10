7f2f7ed762e56980235bd37d688704e143f8578b
mohammad <habibollahi24@gmail.com>
2022-09-10 18:45:42 +0430

    change continues :)))))))))))))))))))))))

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const menuBtn = document.querySelector(".menu-btn");
 const navbarMobile = document.querySelector(".navbar-mobile");
 const navigationMobile = document.querySelector(".navigation-mobile");
 const my_work_on_page = document.getElementById("my-work-on-page");
-console.log(my_work_on_page);
 const secondLine = document.querySelector(".second-line");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navigationMobile.classList.add("onscroll");
+  } else {
+    navigationMobile.classList.remove("onscroll");
+  }
+});
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("animation-btn");
   navbarMobile.classList.toggle("animation-nav");
